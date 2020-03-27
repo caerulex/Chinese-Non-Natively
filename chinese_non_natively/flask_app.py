3d9810ecd9a50cfa43ed5bc0da65e409fd4be14f
Caerulex <sarii@caerulex.in>
2020-03-27 01:45:57 -0400

    trying to fix adding upload form

--- chinese_non_natively/flask_app.py
+++ chinese_non_natively/flask_app.py
@@ -18,6 +18,20 @@ temp_hash = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercas
 UPLOAD_FOLDER = dir_path +  '/' + temp_hash
 ALLOWED_EXTENSIONS = {'txt', 'csv'}
 
+page_head = "<a name=top><div id=anchor_0><h1>Chinese Non-Natively</h1> \
+	<h2 id=fancy_chinese>让中文浅显易懂</h2></div></a>"
+upload_form = '''
+	<h1>Upload new File</h1>
+	<form method=post enctype=multipart/form-data>
+	<input type=file name=file>
+	<input type=submit value=Upload>
+	</form>
+	'''
+button = '<button name="runButton" onclick="run_button()">Run</button>'
+global text
+text = ""
+
+
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -42,14 +56,13 @@ def upload_file():
 			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 			return redirect(url_for('uploaded_file',
 									filename=filename))
-	else:
-		return 'UPLOAD ERROR'
 
 def run_button():
 	reader = ChineseLanguageAssistantReader(raw_chinese_files_dir = UPLOAD_FOLDER)
 	csv_files = sorted(glob.glob(UPLOAD_FOLDER + '/*.csv'))
 	reader.load_dict(UPLOAD_FOLDER + '/' + csv_files[0])
-	text = reader.wrap_raw_text_with_english_and_pinyin(show_pinyin=show_pinyin,
+	global text
+	text += reader.wrap_raw_text_with_english_and_pinyin(show_pinyin=show_pinyin,
 														show_definitions=show_definitions,
 														hide_non_vocab_pinyin=pinyin_only_on_defs)
 	## Try to remove temp folder; if failed show an error using try...except on screen
@@ -57,16 +70,14 @@ def run_button():
 		shutil.rmtree(UPLOAD_FOLDER)
 	except OSError as e:
 		print ("Error: %s - %s." % (e.filename, e.strerror))
-	
-	return text
+	return redirect(request.url)
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
 	style = html_definitions.get_style(hide_non_vocab_pinyin=hide_non_vocab_pinyin, base_font_size=base_font_size, \
 		english_scaling=english_scaling, theme=theme)
-	text = "<a name=top><div id=anchor_0><h1>Chinese Non-Natively</h1> \
-			<h2 id=fancy_chinese>让中文浅显易懂</h2></div></a>"
-	text += '<button name="runButton" onclick="run_button()">Run</button>'
 	
-	return '<!DOCTYPE html>' + style + html_definitions.header + "\n" + text + html_definitions.footer
+	return '<!DOCTYPE html>' + style + html_definitions.header + "\n" + \
+		page_head + upload_form + button + \
+			text  + html_definitions.footer
 	
